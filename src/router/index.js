6ac2db2e85cda9ed484b2a24bc07ceb57cab53db
ymleelab <ymleelab@gmail.com>
2022-11-30 14:43:43 +0900

    add initial files

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,23 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import HomeView from '../views/HomeView.vue';
+import Vue from 'vue'
+import Router from 'vue-router'
+import Login from './views/Login.vue'
+import ChatDashboard from './views/ChatDashboard.vue'
 
-const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView,
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue'),
-  },
-];
+Vue.use(Router)
 
-const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes,
-});
-
-export default router;
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '/',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '/chat',
+      name: 'chat',
+      component: ChatDashboard,
+    }
+  ]
+})
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,35 @@
-import { createStore } from 'vuex';
+import Vue from 'vue'
+import Vuex from 'vuex'
+import VuexPersistence from 'vuex-persist'
+import mutations from './mutations'
+import actions from './actions'
 
-export default createStore({
+Vue.use(Vuex)
+
+const debug = process.env.NODE_ENV !== 'production'
+
+const vuexLocal = new VuexPersistence({
+  storage: window.localStorage
+})
+
+export default new Vuex.Store({
   state: {
+    loading: false,
+    sending: false,
+    error: null,
+    user: [],
+    reconnect: false,
+    activeRoom: null,
+    rooms: [],
+    users: [],
+    messages: [],
+    userTyping: null
   },
   getters: {
+    hasError: state => state.error ? true : false
   },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  },
-});
+  mutations,
+  actions,
+  plugins: [vuexLocal.plugin],
+  strict: debug
+})
